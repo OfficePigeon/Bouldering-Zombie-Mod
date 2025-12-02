95aafd5e93fc140e5f6413b0309533999cdf1ae0
OfficePigeon <ava@wich.fun>
2025-12-02 11:15:05 -0500

    Entity model cleanup

--- src/main/java/fun/wich/client/BoulderingZombieEntityModel.java
+++ src/main/java/fun/wich/client/BoulderingZombieEntityModel.java
@@ -14,13 +14,13 @@ public class BoulderingZombieEntityModel extends ZombieEntityModel<ZombieEntityR
 	public static TexturedModelData getTexturedModelData() {
 		ModelData modelData = BipedEntityModel.getModelData(Dilation.NONE, 0);
 		ModelPartData modelPartData = modelData.getRoot();
-		modelPartData.addChild(EntityModelPartNames.HEAD, ModelPartBuilder.create().uv(0, 0).cuboid(-4, -8, -4, 8, 8, 8), ModelTransform.origin(0, 0, 0));
-		modelPartData.addChild(EntityModelPartNames.HAT, ModelPartBuilder.create().uv(32, 0).cuboid(-4, -8, -4, 8, 8, 8, new Dilation(0.5f)), ModelTransform.origin(0, 0, 0));
-		modelPartData.addChild(EntityModelPartNames.BODY, ModelPartBuilder.create().uv(16, 16).cuboid(-4, 0, -2, 8, 12, 4), ModelTransform.origin(0, 0, 0));
+		modelPartData.addChild(EntityModelPartNames.HEAD, ModelPartBuilder.create().uv(0, 0).cuboid(-4, -8, -4, 8, 8, 8), ModelTransform.NONE);
+		modelPartData.addChild(EntityModelPartNames.HAT, ModelPartBuilder.create().uv(32, 0).cuboid(-4, -8, -4, 8, 8, 8, new Dilation(0.5f)), ModelTransform.NONE);
+		modelPartData.addChild(EntityModelPartNames.BODY, ModelPartBuilder.create().uv(16, 16).cuboid(-4, 0, -2, 8, 12, 4), ModelTransform.NONE);
 		modelPartData.addChild(EntityModelPartNames.LEFT_ARM, ModelPartBuilder.create().uv(34, 32).cuboid(-1, -2, -2, 5, 14, 4), ModelTransform.origin(5, 2, 0));
 		modelPartData.addChild(EntityModelPartNames.RIGHT_ARM, ModelPartBuilder.create().uv(16, 32).cuboid(-4, -2, -2, 5, 14, 4), ModelTransform.origin(-5, 2, 0));
-		modelPartData.addChild(EntityModelPartNames.LEFT_LEG, ModelPartBuilder.create().uv(0, 34).cuboid(-1.9F, 0, -2, 4, 12, 4), ModelTransform.origin(1.9F, 12, 0));
-		modelPartData.addChild(EntityModelPartNames.RIGHT_LEG, ModelPartBuilder.create().uv(0, 16).cuboid(-2.1F, 0, -2, 4, 12, 4), ModelTransform.origin(-1.9F, 12, 0));
+		modelPartData.addChild(EntityModelPartNames.LEFT_LEG, ModelPartBuilder.create().uv(0, 34).cuboid(-1.9f, 0, -2, 4, 12, 4), ModelTransform.origin(1.9f, 12, 0));
+		modelPartData.addChild(EntityModelPartNames.RIGHT_LEG, ModelPartBuilder.create().uv(0, 16).cuboid(-2.1f, 0, -2, 4, 12, 4), ModelTransform.origin(-1.9f, 12, 0));
 		return TexturedModelData.of(modelData, 64, 64);
 	}
 }
