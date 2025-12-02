5b9a49b01b071c56f67e74f1bc8a605fa18814ed
OfficePigeon <ava@wich.fun>
2025-12-02 10:54:17 -0500

    Fixed bouldering zombie entity model layers

--- src/main/java/fun/wich/client/BoulderingZombiesClient.java
+++ src/main/java/fun/wich/client/BoulderingZombiesClient.java
@@ -6,21 +6,15 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
-import net.minecraft.client.render.entity.model.BipedEntityModel;
 import net.minecraft.client.render.entity.model.EntityModelLayer;
 import net.minecraft.util.Identifier;
 
 @Environment(EnvType.CLIENT)
 public class BoulderingZombiesClient implements ClientModInitializer {
-	public static final EntityModelLayer BOULDERING_ZOMBIE = MakeModelLayer("bouldering_zombie");
-	public static final EntityModelLayer BOULDERING_ZOMBIE_BABY = MakeModelLayer("bouldering_zombie_baby");
-	private static EntityModelLayer MakeModelLayer(String path) {
-		return new EntityModelLayer(Identifier.of(BoulderingZombiesMod.MOD_ID, path), "main");
-	}
+	public static final EntityModelLayer BOULDERING_ZOMBIE = new EntityModelLayer(Identifier.of(BoulderingZombiesMod.MOD_ID, "bouldering_zombie"), "main");
 	@Override
 	public void onInitializeClient() {
 		EntityModelLayerRegistry.registerModelLayer(BOULDERING_ZOMBIE, BoulderingZombieEntityModel::getTexturedModelData);
-		EntityModelLayerRegistry.registerModelLayer(BOULDERING_ZOMBIE_BABY, () -> BoulderingZombieEntityModel.getTexturedModelData().transform(BipedEntityModel.BABY_TRANSFORMER));
 		EntityRendererRegistry.register(BoulderingZombiesMod.BOULDERING_ZOMBIE, BoulderingZombieEntityRenderer::new);
 	}
 }
